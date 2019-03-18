07337ab76a8583885a943e072d382e1d40fa229b
wfei <wfei@gpu028.aibee.cn>
2019-03-19 07:02:40 +0800

    fix training caffe path

--- examples/lprnet/train.sh
+++ examples/lprnet/train.sh
@@ -2,6 +2,6 @@
 GPU_ID=0,1
 cur_date=`date +%Y-%m-%d-%H-%M-%S`
 log_file_name=./examples/lprnet/log/${cur_date}
-./build/tools/caffe train \
+/root/3rd/warpctc-caffe/build/tools/caffe train \
     -solver ./examples/lprnet/solver.prototxt \
     -gpu $GPU_ID 2>&1 | tee -a ${log_file_name}
