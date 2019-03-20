c53a8a9878cff84802270df6c7a9ad9d7f52c5f0
rodney-wang <wfei@aibee.com>
2019-03-19 22:24:37 -0700

    crnn for plate training

--- examples/crnn/train.sh
+++ examples/crnn/train.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-GPU_ID=0
+GPU_ID=0,1,2,3
 cur_date=`date +%Y-%m-%d-%H-%M-%S`
 log_file_name=./examples/crnn/log/${cur_date}
 /root/3rd/warpctc-caffe/build/tools/caffe train \
     -solver ./examples/crnn/solver.prototxt \
-    -weights /mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model/crnn_captcha_iter_10000.caffemodel \
     -gpu $GPU_ID 2>&1 | tee -a ${log_file_name}
+
+#    -weights /mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model/crnn_captcha_iter_10000.caffemodel \
--- examples/lprnet/train.sh
+++ examples/lprnet/train.sh
@@ -4,5 +4,6 @@ cur_date=`date +%Y-%m-%d-%H-%M-%S`
 log_file_name=./examples/lprnet/log/${cur_date}
 /root/3rd/warpctc-caffe/build/tools/caffe train \
     -solver ./examples/lprnet/solver.prototxt \
-    -weights "/mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model/crnn_captcha_iter_10000.caffemodel" \
     -gpu $GPU_ID 2>&1 | tee -a ${log_file_name}
+
+#    -weights /mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model/crnn_captcha_iter_10000.caffemodel \
