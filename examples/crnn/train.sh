7259cb3c779e25ac10b6c6021310e42f84636cc5
wfei <wfei@gpu028.aibee.cn>
2019-03-21 14:07:59 +0800

    training setting

--- examples/crnn/train.sh
+++ examples/crnn/train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-GPU_ID=1
+GPU_ID=2,3,4 
 cur_date=`date +%Y-%m-%d-%H-%M-%S`
 log_file_name=./examples/crnn/log/${cur_date}
 /root/3rd/warpctc-caffe/build/tools/caffe train \
     -solver ./examples/crnn/solver.prototxt \
-    -gpu $GPU_ID 2>&1 | tee -a ${log_file_name}
-
+    -gpu $GPU_ID \
+    -weights /mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model/crnn_plate_v1.0.caffemodel 2>&1 | tee -a ${log_file_name}
 #    -weights /mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model/crnn_captcha_iter_10000.caffemodel \
