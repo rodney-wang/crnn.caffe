5e90a99759b14adc45d675a16ef2cb6ebe797661
rodney-wang <wfei@aibee.com>
2019-04-08 15:25:07 -0700

    test on a single image

--- examples/crnn/crnn_test.py
+++ examples/crnn/crnn_test.py
@@ -1,48 +1,16 @@
 # -*- coding=utf-8 -*-
-import sys
-#sys.path.append('~/Desktop/crnn.caffe/python')
-import caffe
-import json
-from PIL import Image
-import numpy as np
+import argparse
+from plate_ocr_crnn import PlateOCR
 
-model_file = './model/crnn_plate_iter_12000.caffemodel'
-deploy_file = 'deploy.prototxt'
-test_img = '/mnt/soulfs2/wfei/data/plate.jpg'
 
-# set device
-caffe.set_device(4)
-caffe.set_mode_gpu()
+parser = argparse.ArgumentParser(description='Plate Segmentation')
+parser.add_argument('--img', default='/mnt/soulfs2/wfei/data/plate.jpg',
+                    type=str, help='Input image')
+parser.add_argument('--model',
+                    default='/mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model_k11/crnn_k11_energy_iter_7500.caffemodel',
+                    type=str, help='Caffe model path')
+args = parser.parse_args()
 
-# load model
-net = caffe.Net(deploy_file, model_file, caffe.TEST)
-
-# load test img
-IMAGE_WIDTH, IMAGE_HEIGHT = 96, 32
-img = caffe.io.load_image(test_img, color=False) #load as grayscale
-img = caffe.io.resize(img, (IMAGE_HEIGHT, IMAGE_WIDTH, 1))
-in_ = np.transpose(img, (2, 0, 1))
-
-# 执行上面设置的图片预处理操作，并将图片载入到blob中
-# shape for input (data blob is N x C x H x W), set data
-net.blobs['data'].reshape(1, *in_.shape)
-net.blobs['data'].data[...] = in_
-
-# run net
-net.forward()
-
-# get result
-res = net.blobs['probs'].data
-
-print('result shape is:', res.shape)
-
-# 取出标签文档
-char_dict = json.load(open('../lprnet/utils/carplate.json', 'r'))
-char_set =  {v: k for k, v in char_dict.iteritems()}
-#char_set[73]='-'
-# 取出最多可能的label标签
-for i in range(24):
-    data = res[i, :, :]
-    index = np.argmax(data)
-    #print(index, data[0, index])
-    print char_set[index].encode('utf8')
+pocr = PlateOCR(args.model)
+chars, score = pocr(args.img)
+print chars, score
