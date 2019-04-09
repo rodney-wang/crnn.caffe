0fffe60f5f96e37fdce12a8cc87f81acaa6bda88
rodney-wang <wfei@aibee.com>
2019-04-09 00:10:04 -0700

    end to end evaluation from ocrtxt label

---
+++ examples/crnn/crnn_eval.py
@@ -0,0 +1,64 @@
+import os
+import glob
+import time
+import argparse
+from eval_generic import eval
+from plate_ocr_crnn import PlateOCR
+
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+"""
+ Process all plates in benchmark
+"""
+
+def run_crnn_and_write_result(plate_file, out_dir, pocr):
+    plate_file = plate_file.strip()
+
+    if not os.path.exists(plate_file.encode('utf-8')):
+        print('File does not exist')
+        return
+    chars, score = pocr(plate_file)
+
+    if score >=0 and len(chars) != 0:
+        fname = os.path.basename(plate_file).split('_plate')[0]
+        fname = fname.replace('.jpg', '.txt')
+
+        out_file = os.path.join(out_dir, fname)
+
+        out_str = ' '.join([chars, str(score)])
+        print fname, out_str
+        # with open(out_file, 'w', encoding='utf-8') as ff:
+        with open(out_file, 'w') as ff:
+            #ff.write(out_str.encode('utf-8'))
+            ff.write(out_str)
+    return True
+
+
+def crnn_eval_end2end(ocrtxt_file, out_dir, model_path, skip):
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    pocr = PlateOCR(model_path)
+    start_time = time.time()
+    for line in open(ocrtxt_file, 'r'):
+        plate_file, label = line.split(';')
+        run_crnn_and_write_result(plate_file, out_dir, pocr, ext)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    eval(ocrtxt_file, out_dir, skip)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Plate Segmentation')
+    parser.add_argument('--ocr_txt', default='/ssd/wfei/data/plate_for_label/energy_cars/energy_plates_ocrlabel_test_217.txt',
+                        type=str, help='Input test image dir')
+    parser.add_argument('--out_dir', default='/ssd/wfei/data/ocr_results/energy_plates_res',
+                        type=str, help='Output image dir')
+    parser.add_argument('--model', default='/mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model_k11/crnn_k11_energy_v1.3.caffemodel', type=str, help='Caffe model path')
+    parser.add_argument('--skip', default=0, type=int, help='Skip any characters in evaluation')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    crnn_eval_end2end(args.ocr_txt, args.out_dir, args.model, args.skip)
---
+++ examples/crnn/eval_generic.py
@@ -0,0 +1,95 @@
+from glob import glob
+from os.path import basename
+import argparse
+
+def test(gts, test_folder, threshold, skip):
+    dets_txt = glob(test_folder + '/*')
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for dets_name in dets_txt:
+        line = open(dets_name).readline()
+        if len(line) == 0:
+            continue
+        #print line
+        carplate, score = line.split()[:2]
+        carplate = carplate.strip()
+        score = float(score)
+        carplate = carplate.decode('utf8')
+        carplate = carplate[skip:]
+        name = basename(dets_name)
+        gt = gts[name]
+        #print gt, len(gt), carplate, len(carplate)
+        if score > threshold:
+            if carplate== gt: #.encode('utf8'):
+                tp += 1
+            else:
+                fp += 1
+    #fn = len(gts) - tp - fp
+    fn = len(dets_txt) - tp - fp
+    if tp +fp ==0:
+        prec =0
+    else:
+        prec = float(tp) / float(tp + fp)
+    if tp +fn ==0:
+        recall=0
+    else:
+        recall = float(tp) / float(tp + fn)
+    return prec, recall
+
+
+def load_gt(ocrtxt_file, skip):
+    gts ={}
+    for line in open(ocrtxt_file, 'r'):
+        fname, label = line.split(';')
+        bname = basename(fname).replace('_plate.jpg', '')
+
+        plate = label.strip().decode('utf8')
+        plate = plate.replace('|', '')
+        #print bname, plate
+        gts[bname] = plate[skip:]
+    print "Total number of gt", len(gts)
+    return gts
+
+def eval(ocrlabel, res_dir, skip):
+    gts = load_gt(ocrlabel, skip)
+    # print gts
+    tmax = 10.0
+    tmin = 0.0
+    tnow = 0.0
+    while True:
+        prec, recall = test(gts, res_dir, tnow, skip)
+        if abs(recall - 0.9) < 0.001:
+            break
+        if recall > 0.9:
+            tmin = tnow
+            tnow = (tnow + tmax) * 0.5
+        if recall < 0.9:
+            tmax = tnow
+            tnow = (tnow + tmin) * 0.5
+        if abs(tmax - tmin) < 0.001:
+            break
+        print(tnow, test(gts, res_dir, tnow, skip))
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Evaluation OCR results')
+    parser.add_argument('--ocrlabel', default='/ssd/wfei/data/plate_for_label/energy_cars/energy_plates_ocrlabel_test_217.txt',
+                        type=str, help='Output plate label dir')
+    parser.add_argument('--res_dir', default='/ssd/wfei/data/plate_for_label/energy_cars/20190124/results/k11_energy_caffek11',
+    #parser.add_argument('--res_dir', default='k11_caffe_v1.1',
+                        type=str, help='Input test image dir')
+    parser.add_argument('--skip', default=0, type=int, help='Skip any characters in evaluation')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    print "Skip {} characters for evaluation!!!".format(skip)
+
+    #res_base = '/ssd/wfei/data/benchmark/k11/'
+    #res_dir = join(res_base, args.res_dir)
+    eval(args.ocrlabel, args.res_dir, args.skip)
