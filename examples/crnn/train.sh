3f27d1d857f29853a761f21bb5d3afa7c02e5441
wfei <wfei@gpu028.aibee.cn>
2019-07-30 13:54:29 +0800

    change hongkong training params

--- examples/crnn/train.sh
+++ examples/crnn/train.sh
@@ -5,5 +5,5 @@ cur_date=`date +%Y-%m-%d-%H-%M-%S`
 log_file_name=./examples/crnn/log/${cur_date}
 /root/3rd/warpctc-caffe/build/tools/caffe train \
     -solver ./examples/crnn/solver.prototxt \
-    -gpu $GPU_ID    -weights /ssd/wfei/code/crnn.caffe/examples/crnn/model_hk/crnn_hkonly_iter_80000.caffemodel 2>&1 | tee -a ${log_file_name} 
+    -gpu $GPU_ID    -weights /ssd/wfei/code/crnn.caffe/examples/crnn/model_hk/crnn_hkonly_v1.1.caffemodel 2>&1 | tee -a ${log_file_name} 
 #    -weights /mnt/soulfs2/wfei/models/crnn_caffe/wanda/crnn_wanda_v2.0.caffemodel 2>&1 | tee -a ${log_file_name}
