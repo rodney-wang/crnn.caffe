68dfe5152c2eae32c43216f875d62d023b39ea5e
wfei <wfei@gpu028.aibee.cn>
2019-08-17 13:29:06 +0800

    train fresh wanda

--- examples/crnn/train.sh
+++ examples/crnn/train.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-GPU_ID=2,3 
-#GPU_ID=1 
+GPU_ID=3 
 cur_date=`date +%Y-%m-%d-%H-%M-%S`
 log_file_name=./examples/crnn/log/${cur_date}
 /root/3rd/warpctc-caffe/build/tools/caffe train \
     -solver ./examples/crnn/solver.prototxt \
-    -gpu $GPU_ID \
-    -weights /mnt/soulfs2/wfei/code/crnn.caffe/examples/crnn/model_wanda/crnn_wanda_iter_25000.caffemodel 2>&1 | tee -a ${log_file_name} 
+    -gpu $GPU_ID | tee -a ${log_file_name} 
+    #-weights /ssd/wfei/models/crnn_caffe/wanda/crnn_wanda_fresh_v2.2.caffemodel 2>&1 | tee -a ${log_file_name} 
 #    -weights /mnt/soulfs2/wfei/models/crnn_caffe/wanda/crnn_wanda_v2.0.caffemodel 2>&1 | tee -a ${log_file_name}
